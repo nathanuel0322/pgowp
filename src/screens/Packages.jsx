3fb1f58c4c686b7673fee8bc3eadb51b02e4a2a5
Nathanuel Dixon <nathan0322@hotmail.com>
2023-09-01 13:03:07 -0400

    Removed prices

--- src/screens/Packages.jsx
+++ src/screens/Packages.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import BackgroundImage from '../assets/images/Rectangle.jpg';
 import '../assets/css/packages.css'
 
 export default function Packages(){
@@ -11,12 +10,9 @@ export default function Packages(){
                     <ul id="liul">
                         <li>Starting Packages</li> 
                         <br />
-                        <li>*Prices subject to change in the near future*</li> 
-                        <br />
-                        <li>2 Hour Video Gaming Party - $500</li> <br />
-                        <li>3 Hours - $650</li> <br />
-                        <li>Each Additional Hour - $150</li> <br />
-                        <li>Laser Tag Party! - $700</li>
+                        <li>2 Hour Video Gaming Party</li> <br />
+                        <li>3 Hour Video Gaming Party</li> <br />
+                        <li>Laser Tag Party!</li>
                         <li style={{color: '#FF9800'}}>3-Hour Movie Package (Contact for More Info)</li> 
                         <li style={{color: '#FF2F2F'}}>*Birthday Child receives a Free Birthday T-Shirt!</li>
                         <li>All Party Attendants receive Prestigious Gaming Wristbands!  </li>
@@ -30,7 +26,6 @@ export default function Packages(){
                         <li style={{color: '#FF007F'}}>Cotton Candy Machine!</li>
                         <li style={{marginTop: 20, textDecoration: 'underline'}} >For a More Fun and Exciting Party, You can also add:</li>
                         <li style={{color: '#FF5722'}}>Laser Tag with Live Scoring!</li>
-                        <li style={{color: '#FF5722'}}>Additional $20 per person for two 15-minute sessions!</li>
                         <li style={{color: '#8778C4'}}>Karaoke!</li>
                         <li style={{color: '#E91E63'}}>Hot Dog Grill!</li>
                         <li style={{color: '#05ff69'}}>Yard Games!</li>
