93676f3fa7dd3e20af87a70bb48ad3d35be7e70b
Nathanuel Dixon <nathan0322@hotmail.com>
2024-06-16 12:23:23 -0400

    chore: Update favicon link and background color in index.html

--- client/src/components/home/StarSet.jsx
+++ client/src/components/home/StarSet.jsx
@@ -1,31 +1,42 @@
-import Star from '../../assets/icons/star.svg';
-import EmptyStar from '../../assets/icons/emptystar.svg';
-import HalfStar from '../../assets/icons/halfstar.svg';
+import Star from "../../assets/icons/star.svg";
+import EmptyStar from "../../assets/icons/emptystar.svg";
+import HalfStar from "../../assets/icons/halfstar.svg";
 
-export default function StarSet({num}) {
-    return (
-        <div title="RatingBar__Container" className="ecFtME">
-            {[1, 2, 3, 4, 5].map((star, index) => {
-                return (
-                    <div title="RatingItemFilledSvg__Container" className="hoAzGt es-rating-stars-item-filled" key={index}>
-                        {num >= star ?
-                            <div title="RatingItemFilledSvg__ContainerAbsolute RatingItemFilledSvg__Filled" className="chMKQB bFQRJO">
-                                <img src={Star} alt="filledstar" />
-                            </div>
-                        :
-                        num === star - 0.5 ?
-                            <div title="RatingItemFilledSvg__ContainerAbsolute RatingItemFilledSvg__Half" className="chMKQB bFQRJO">
-                                <img src={HalfStar} alt="filledstar" />
-                            </div>
-                        :
-                        num < star - 0.5 &&
-                            <div title="RatingItemFilledSvg__ContainerAbsolute RatingItemFilledSvg__Empty" className="chMKQB biFvfu">
-                                <img src={EmptyStar} alt="filledstar" />
-                            </div>
-                        }
-                    </div>
-                )
-            })}
+export default function StarSet({ num }) {
+  return (
+    <div title="RatingBar__Container" className="ecFtME">
+      {[1, 2, 3, 4, 5].map((star, index) => (
+        <div
+          title="RatingItemFilledSvg__Container"
+          className="hoAzGt es-rating-stars-item-filled"
+          key={index}
+        >
+          {num >= star ? (
+            <div
+              title="RatingItemFilledSvg__ContainerAbsolute RatingItemFilledSvg__Filled"
+              className="chMKQB bFQRJO"
+            >
+              <img src={Star} alt="filledstar" />
+            </div>
+          ) : num === star - 0.5 ? (
+            <div
+              title="RatingItemFilledSvg__ContainerAbsolute RatingItemFilledSvg__Half"
+              className="chMKQB bFQRJO"
+            >
+              <img src={HalfStar} alt="filledstar" />
+            </div>
+          ) : (
+            num < star - 0.5 && (
+              <div
+                title="RatingItemFilledSvg__ContainerAbsolute RatingItemFilledSvg__Empty"
+                className="chMKQB biFvfu"
+              >
+                <img src={EmptyStar} alt="filledstar" />
+              </div>
+            )
+          )}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
