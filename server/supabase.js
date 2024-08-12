76c522ef5e5da00a09709725e0762f032a1e1388
Nathanuel Dixon <nathan0322@hotmail.com>
2024-08-11 22:38:23 -0400

    big changes - all code moved into root folder

--- server/supabase.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const supabase = createClient("https://xjmbjuqypyphckrgpeib.supabase.co", process.env.SUPABASE_KEY);
-
-module.exports = { supabase };
