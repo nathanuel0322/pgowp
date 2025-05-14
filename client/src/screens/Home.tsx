18936cf26be5d8c5e857ff7cd8f9c0004b529dfe
Your Name <nathan0322@hotmail.com>
2025-05-13 23:58:58 -0400

    Update line-clamp value for review text in Home component
    Adjusted the line-clamp value for the review text in the Home
    component to improve text visibility and layout. The change
    modifies the line-clamp from 7 to 5 for screens with a minimum
    width of 600px, ensuring better readability of reviews.

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -456,7 +456,7 @@ export default function Home() {
                                                                             <i className="fas fa-quote-left gradient-icon"></i>
                                                                         </span>
                                                                     </div>
-                                                                    <p className="item-text fonts-style display-4 line-clamp-[11] atleast600:line-clamp-[7]">
+                                                                    <p className="item-text fonts-style display-4 line-clamp-[11] atleast600:line-clamp-5">
                                                                         {review.reviewtext}
                                                                     </p>
                                                                 </div>
